b0fbe8d938e0ae8c512de52e720fcdaf99879811
novastosha <galliigotu@gmail.com>
2021-11-21 20:23:44 +0100

    Initial commit

---
+++ src/dev/nova/hell/menus/menu/manager/MenuManager.java
@@ -0,0 +1,14 @@
+package dev.nova.hell.menus.menu.manager;
+
+public final class MenuManager {
+
+    /**
+     *
+     * Menu manager cannot be instanced!
+     *
+     */
+    private MenuManager() { throw new AssertionError("MenuManager cannot be instanced!"); }
+
+
+
+}
---
+++ src/dev/nova/hell/menus/plugin/HellMenusPlugin.java
@@ -0,0 +1,12 @@
+package dev.nova.hell.menus.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HellMenusPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
